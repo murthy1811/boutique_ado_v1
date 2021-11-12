1587c4f8561d41d88a7ee13386d169adac3c115f
Murthy Valavala <murthy.mailme@gmail.com>
2021-11-12 19:44:23 +0000

    added stripe function

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -10,10 +14,16 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jv4enLQYZnHOv8vmdugIUuQEuGzmQaG8OI9kr4UKPej6NA5RNiu2x4ZqUojls4OJA9ACEcFdw0RuWbKtosGe1nB00xWpRbNil',
+        'client_secret': 'test_secret_key',
     }
 
     return render(request, template, context)
