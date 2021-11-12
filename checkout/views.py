dae3934715ac9ce7fa1d73640593e04749fd5a61
Murthy Valavala <murthy.mailme@gmail.com>
2021-11-12 20:49:48 +0000

    stripe card payment functioanlity work in progress

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -17,13 +20,20 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Jv4enLQYZnHOv8vmdugIUuQEuGzmQaG8OI9kr4UKPej6NA5RNiu2x4ZqUojls4OJA9ACEcFdw0RuWbKtosGe1nB00xWpRbNil',
-        'client_secret': 'test_secret_key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': client_secret,
     }
 
     return render(request, template, context)
