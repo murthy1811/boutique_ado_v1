8f0ecdcc0c5834fb48570dff7705025b7f3b9c32
Murthy Valavala <murthy.mailme@gmail.com>
2021-11-14 23:00:38 +0000

    completed confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,9 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
